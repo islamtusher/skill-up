874b91f2fc7e99a859712d40fa65461ee2348213
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-08-29 15:40:42 +0600

    add navbar on top

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HeroArea from './Components/HeroArea/HeroArea';
+import Navbar from './Components/Shared/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <HeroArea></HeroArea>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<HeroArea></HeroArea>}></Route>
+      </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#50d8fa',
+      },
+    }
   },
   plugins: [require("daisyui")]
 }
