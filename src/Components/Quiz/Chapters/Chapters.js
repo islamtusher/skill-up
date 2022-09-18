06ee6fea4e3b6e3c731693b1eeb2bee9899fdeee
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-09-18 10:35:45 +0600

    handle back to the previous question

--- src/Components/Quiz/Chapters/Chapters.js
+++ src/Components/Quiz/Chapters/Chapters.js
@@ -8,29 +8,29 @@ import { useParams } from 'react-router-dom';
 
 const Chapters = () => {
     const {id, subject} = useParams()
-    const { handleSubmit, register, reset } = useForm(); // react form hook
+    const { handleSubmit, register, reset, formState: { errors } } = useForm(); // react form hook
 
     const [currentChapter, setCurrentChapter] = useState(1)
     const [questionNo, setQuestionNo] = useState(0)
     const [selectedAns, setSelectedAns] = useState([])
     const [isSubmit, setIsSubmit] = useState(false)
     const [submitSuccess, setSubmitSuccess] = useState(false)
-    const [lastAns, setLastAns] = useState({})
+    const [lastAns, setLastAns] = useState(0)
+    const [isChecked, setIsChecked] = useState(true)
 
     // never Used
     const [isActive, setIsActive] = useState(false)
-
     // fake data Chapters
     const chapters = [
-        {id:1, chapterName:"1th"},
-        {id:2, chapterName:"2th"},
-        {id:3, chapterName:"3th"},
+        {id:1, chapterName:"1st"},
+        {id:2, chapterName:"2nd"},
+        {id:3, chapterName:"3rd"},
         {id:4, chapterName:"4th"},
         {id:5, chapterName:"5th"},
-        // {id:6, chapterName:"6th"},
-        // {id:7, chapterName:"7th"},
-        // {id:8, chapterName:"8th"},
-        // {id:9, chapterName:"9th"},
+        {id:6, chapterName:"6th"},
+        {id:7, chapterName:"7th"},
+        {id:8, chapterName:"8th"},
+        {id:9, chapterName:"9th"},
     ]
 
     const allChapterQuestions = [
@@ -69,7 +69,6 @@ const Chapters = () => {
             {id:4, question:"This is the Question No 4", options:[{optionId: 1, option: "option-1"}, {optionId: 2, option: "option-2"}, {optionId: 3, option: "option-3"}]},
             {id:5, question:"This is the Question No 5", options:[{optionId: 1, option: "option-1"}, {optionId: 2, option: "option-2"}]},
         ]
-
     ]
 
     // Handle chapter list click
@@ -79,15 +78,17 @@ const Chapters = () => {
         setIsSubmit(false)
         setSubmitSuccess(false)
         reset()
-        
     }
 
     // handle Previous Btn
     const handlePreviousBtn = data => {
         setQuestionNo((count) => count - 1)
-        setLastAns(selectedAns[selectedAns.length-1])
-        // console.log(selectedAns[selectedAns.length-1]);
-        
+        selectedAns.pop()
+        setIsSubmit(false)
+
+        // setIsChecked(!isChecked)
+        // setLastAns(selectedAns[selectedAns.length-1].answer)
+        console.log(selectedAns)
     };
 
     // set Submit btn on last question
@@ -100,17 +101,21 @@ const Chapters = () => {
     // handle from submition
     const onSubmit = data => { 
         if (isSubmit) {
-            setSelectedAns([...selectedAns, data])
-            setSubmitSuccess(true)
+            setSelectedAns([...selectedAns, {qestionNo: questionNo + 1, answer : data.option}]) // Push ans on ans-array
+            setSubmitSuccess(true) // for success message
             return
         }
-        setQuestionNo(questionNo + 1)
-        setSelectedAns([...selectedAns, data])
-        // console.log(questionNo);
+        setQuestionNo(questionNo + 1) // read questons one by one
+        setSelectedAns([...selectedAns, {qestionNo: questionNo + 1, answer : data.option}])  // Push ans on ans-array
         reset()
     };
-    console.log(lastAns);
-
+    
+    const handleCheckBox = (e) => {
+        console.log(e.target.value);
+        console.log('change');
+        setIsChecked(!isChecked)
+    }
+    console.log(currentChapter)
     return (
         <div className='min-h-screen mx-auto font-["jost"] bg-slate-100 pt-[100px] pb-[30px]'>
             <div className="flex flex-col lg:flex-row justify-around lg:items-start ">
@@ -138,7 +143,7 @@ const Chapters = () => {
                     </h1>
                     <div className="h-[550px] bg-white rounded shadow-2xl border-primary py-10 px-36">
                         {
-                            submitSuccess ?
+                            submitSuccess ? // show mcq done successflly
                                 <div className="flex flex-col items-center justify-center">
                                     <img src="../images/tropy.jpg" className='w-60' alt="" />
                                     <h1 className='text-2xl font-bold mb-4'>
@@ -146,7 +151,7 @@ const Chapters = () => {
                                         <br/> <span className='text-green-500'>Successfully Done</span>
                                     </h1>
                                     <button className='btn btn-primary capitalize'> See Correct Answare</button>
-                                    <p className='w-[210px] font-[jost] flex items-center justify-between mt-4 border-b-2 border-zinc-500 hover:text-primary hover:border-primary hover:cursor-pointer transition ease-in-out duration-300'>
+                                    <p onClick={()=>handleChapter(currentChapter + 1)} className='w-[210px] font-[jost] flex items-center justify-between mt-4 border-b-2 border-zinc-500 hover:text-primary hover:border-primary hover:cursor-pointer transition ease-in-out duration-300'>
                                         <span className='text-lg tracking-[2px]'>Next Chapter MCQ</span>
                                         <FontAwesomeIcon className=' text-sm mt-1' icon={faArrowRightLong} />
                                     </p>
@@ -161,20 +166,27 @@ const Chapters = () => {
                                     <div className="flex flex-col justify-self-start gap-y-4"> 
                                         {/* Loop on Options  */}
                                         {
-                                            allChapterQuestions[currentChapter - 1][questionNo]?.options.map((opt) =>
+                                            allChapterQuestions[currentChapter - 1][questionNo]?.options.map((opt) =>                                            
                                                 <label key={opt.optionId} className=" cursor-pointer flex items-center justify-evenly text-lg border-2 bg-slate-100 hover:bg-slate-200 rounded p-4">
                                                     <input
                                                         type="radio"
+                                                        // checked= {opt.optionId === parseInt(lastAns) ? true : false}
+                                                        // onChange={handleCheckBox}
                                                         id={allChapterQuestions[currentChapter -1][questionNo]?.id}
-                                                        defaultValue={opt.optionId + 1}
+                                                        defaultValue={opt.optionId}
                                                         className="radio mr-3 radio-primary bg-white "
-                                                        {...register(`question-${allChapterQuestions[currentChapter -1][questionNo]?.id}`)}
-                                                        
+                                                        {...register(`option`, {
+                                                            required: {
+                                                                value: true,
+                                                                message: "Please Select an Option"
+                                                            }
+                                                        })}                                                                                                                
                                                     />                                
                                                     <span className="font-bold">{opt.option}</span> 
-                                                </label> 
+                                                </label>
                                             )
                                         }   
+                                        <p className='text-red-500'>{errors.option?.message}</p>
                                         <div className="flex items-center justify-between gap-x-4">     
                                             {/* Previous Button  */}
                                             {
@@ -189,13 +201,8 @@ const Chapters = () => {
                                                 <span className='text-lg tracking-[2px]'>{isSubmit ? "Submit" : "Next"}</span>
                                                 <FontAwesomeIcon className=' text-sm mt-1' icon={faArrowRightLong} />
                                             </button>
-
-                                            {/* <button type="submit" className='btn btn-primary'>
-                                                {isSubmit ? "Submit" : "Next"}                                
-                                            </button> */}
                                         </div>
-                                    </div> 
-                                    
+                                    </div>                                     
                                 </form>
                         }
                     </div>
